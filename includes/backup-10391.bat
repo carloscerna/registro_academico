90bc384e154c8eb0cea634eb4d6ec8ca5ef61d8c
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2022-10-04 15:17:20 -0600

    update-

--- includes/backup-10391.bat
+++ includes/backup-10391.bat
@@ -4,3 +4,5 @@ pg_dump -U postgres -v -F c registro_academico_10391 > c:\wamp64\www\registro_ac
 cls
 xcopy c:\wamp64\www\registro_academico_10391.dump "E:\Mi unidad\CE10391\respaldo" /y
 xcopy c:\wamp64\www\registro_academico_10391.dump "D:\CE10391\respaldo" /y
+
+xcopy c:\wamp64\www\registro_academico_10391.dump "C:\Users\MINEDUCYT\OneDrive - Ministerio de Educación, Gobierno de El Salvador\CE10391\Respaldo" /Y
