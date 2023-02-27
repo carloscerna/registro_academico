c3bcfa9987b7de8c7d9e7ad00d9b7e74b9a144e8
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2023-02-27 10:13:50 -0600

    update-

--- php_libs/reportes/nomina_asistencia_.php
+++ php_libs/reportes/nomina_asistencia_.php
@@ -90,7 +90,7 @@ function FancyTable($header)
     $this->SetLineWidth(.3);
     $this->SetFont('','B');
     //Cabecera
-    $w=array(10,80,170); //determina el ancho de las columnas
+    $w=array(6,14,70,170); //determina el ancho de las columnas
     $w1=array(5.66); //determina el ancho de las columnas
     
     for($i=0;$i<count($header);$i++)
@@ -104,9 +104,9 @@ function FancyTable($header)
               $this->Ln();
         
             $this->Cell($w[0],7,'','LBR',0,'C',1);
-        
+            $this->Cell($w[1],7,'','LBR',0,'C',1);
           
-        $this->Cell($w[1],7,'(Orden Alfabético por Apellido)','LBR',0,'C',1);
+        $this->Cell($w[2],7,'(Orden Alfabético por Apellido)','LBR',0,'C',1);
 
     $this->SetFillColor(255,255,255);
     for($j=0;$j<=29;$j++)
@@ -129,7 +129,7 @@ function FancyTable($header)
     #Establecemos el margen inferior: 
     $pdf->SetAutoPageBreak(true,10);
 //Títulos de las columnas
-    $header=array('Nº','Nombre de Alumnos/as');
+    $header=array('Nº','NIE','Nombre de Alumnos/as');
     $pdf->AliasNbPages();
     $pdf->SetFont('Arial','',12);
     $pdf->AddPage();
@@ -145,15 +145,18 @@ function FancyTable($header)
     $pdf->SetFont('Arial','',9); // I : Italica; U: Normal;
 
     $pdf->FancyTable($header); // Solo carge el encabezado de la tabla porque medaba error el cargas los datos desde la consulta.
-    $w=array(10,80,5.66); //determina el ancho de las columnas
+    $w=array(6,14,70,5.66); //determina el ancho de las columnas
     
     $fill=false; $i=1; $m = 0; $f = 0; $suma = 0;
         while($row = $result -> fetch(PDO::FETCH_BOTH))
             {
-            $pdf->Cell($w[0],6,$i,'LR',0,'C',$fill);        // núermo correlativo
-            $pdf->Cell($w[1],6,utf8_decode(trim($row['apellido_alumno'])),'LR',0,'L',$fill); // Nombre + apellido_materno + apellido_paterno
+                $pdf->SetFont('Arial','',8); // I : Italica; U: Normal;
+                    $pdf->Cell($w[0],6,$i,'LR',0,'C',$fill);        // núermo correlativo
+                    $pdf->Cell($w[1],6,trim($row['codigo_nie']),'LR',0,'C',$fill);        // núermo correlativo
+                    $pdf->Cell($w[2],6,utf8_decode(trim($row['apellido_alumno'])),'LR',0,'L',$fill); // Nombre + apellido_materno + apellido_paterno
+                $pdf->SetFont('Arial','',9); // I : Italica; U: Normal;
             for($j=0;$j<=29;$j++)
-                $pdf->Cell($w[2],6,'','1',0,'C',$fill);
+                $pdf->Cell($w[3],6,'','1',0,'C',$fill);
                     $pdf->ln();
 
                 if($i==25 || $i == 51 || $i == 65){
@@ -177,16 +180,17 @@ function FancyTable($header)
                   {
                       $pdf->Cell($w[0],6,$menor_de_25++,'LR',0,'C',$fill);  // N| de Orden.
                       $pdf->Cell($w[1],6,'','LR',0,'l',$fill);  // nombre del alumno.
+                      $pdf->Cell($w[2],6,'','LR',0,'l',$fill);  // nombre del alumno.
 
 			for($j=0;$j<=29;$j++)
-                	$pdf->Cell($w[2],6,'','1',0,'C',$fill);
+                	$pdf->Cell($w[3],6,'','1',0,'C',$fill);
                 
                       $pdf->Ln();   
                       $fill=!$fill;
                       
                       // Salto de Línea.
         		if($i==25 || $i == 51 || $i == 65){
-				$pdf->Cell(array_sum($w)+$w[2]*29,0,'','T');
+				$pdf->Cell(array_sum($w)+$w[3]*29,0,'','T');
 				$pdf->AddPage();
 				$pdf->FancyTable($header);}
                   }
@@ -200,22 +204,23 @@ function FancyTable($header)
                   {
                       $pdf->Cell($w[0],6,$numero++,'LR',0,'C',$fill);  // N| de Orden.
                       $pdf->Cell($w[1],6,'','LR',0,'l',$fill);  // nombre del alumno.
+                      $pdf->Cell($w[2],6,'','LR',0,'l',$fill);  // nombre del alumno.
 
 			for($j=0;$j<=29;$j++)
-                	$pdf->Cell($w[2],6,'','1',0,'C',$fill);
+                	$pdf->Cell($w[3],6,'','1',0,'C',$fill);
                 
                       $pdf->Ln();   
                       $fill=!$fill;
                       
                       // Salto de Línea.
         		if($i==25 || $i == 51 || $i == 65){
-				$pdf->Cell(array_sum($w)+$w[2]*29,0,'','T');
+				$pdf->Cell(array_sum($w)+$w[3]*29,0,'','T');
 				$pdf->AddPage();
 				$pdf->FancyTable($header);}
                   }
            }    // IF QUE AGREGAR LAS LINEAS FALTANTES.
            
-$pdf->Cell(array_sum($w)+$w[2]*29,0,'','T');
+$pdf->Cell(array_sum($w)+$w[3]*29,0,'','T');
 // Salida del pdf.
 	$modo = 'I'; // Envia al navegador (I), Descarga el archivo (D).
 	$print_nombre = trim($print_grado) . ' ' . trim($print_seccion) .'.pdf';
