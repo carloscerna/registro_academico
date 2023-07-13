0f409d38cbb1159b69b58450fde9a75de50640db
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2023-07-13 15:30:49 -0600

    update-bootstrap-5

---
+++ js/main_servicio_educativo.js
@@ -0,0 +1,14 @@
+$(function(){
+
+}); // FIN DEL FUNCTION.
+
+// Mensaje de Carga de Ajax.
+function configureLoadingScreen(screen){
+    $(document)
+        .ajaxStart(function () {
+            screen.fadeIn();
+        })
+        .ajaxStop(function () {
+            screen.fadeOut();
+        });
+    }
