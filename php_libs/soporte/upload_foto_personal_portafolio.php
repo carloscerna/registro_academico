0d1dfce39dbf960a7866c761a96e619437562ba3
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2023-09-20 15:43:31 -0600

    update-varios

--- php_libs/soporte/upload_foto_personal_portafolio.php
+++ php_libs/soporte/upload_foto_personal_portafolio.php
@@ -1,23 +1,30 @@
 <?php
 // ruta de los archivos con su carpeta
     $path_root=trim($_SERVER['DOCUMENT_ROOT']);
+// Incluimos el archivo de funciones y conexión a la base de datos
+    include($path_root."/acomtus/includes/mainFunctions_conexion.php");
 // URL PARA GUARDAR LAS IMAGENES.
-    $url_ = "/acomtus/img/portafolio/";
+    $url_ = "/registro_academico/img/portafolio/";
     $small = 'thumbails/';
     $large = 'large/';
     $random = rand();
+    $respuestaOK = true;
+    $url_archivo = "";
+    $mensajeError = "No hay archivo.";
+    $contenidoOK = "";
 // verificar si existe la imagen.
 if (is_array($_FILES) && count($_FILES) > 0) {
     if (($_FILES["file"]["type"] == "image/pjpeg")
         || ($_FILES["file"]["type"] == "image/jpeg")
         || ($_FILES["file"]["type"] == "image/png")
-        || ($_FILES["file"]["type"] == "image/gif")) {
+        || ($_FILES["file"]["type"] == "image/gif") 
+        || ($_FILES["file"]["type"] == "image/jpg")
+        || ($_FILES['file']['type']) == 'application/pdf') {
         if (move_uploaded_file($_FILES["file"]["tmp_name"], $path_root . $url_ . $_FILES['file']['name'])) {
-            // Incluimos el archivo de funciones y conexión a la base de datos
-                include($path_root."/acomtus/includes/mainFunctions_conexion.php");
             // Variables de la imagen.
                 $id_personal = $_SESSION["id_personal"];
                 $id_portafolio = $_SESSION['id_portafolio'];
+                $codigo_institucion = $_SESSION["codigo_institucion"];
             // Armamos el query PARA ELIMINAR LA IMAGEN O SEA EL ARCHIVO.
                 $query_file = "SELECT * FROM personal_portafolio WHERE id_ = $id_portafolio";
             // Ejecutamos el query
@@ -69,7 +76,17 @@ if (is_array($_FILES) && count($_FILES) > 0) {
                         $original = imagecreatefromjpeg($path_root.$url_.$id_personal."/".$nombreArchivo);
                     }else if($_FILES["file"]["type"] == "image/png"){
                         $original = imagecreatefrompng($path_root.$url_.$id_personal."/".$nombreArchivo);
+                    }else if(($_FILES['file']['type']) == 'application/pdf'){
+                        $mensajeError = "Cargado Archivo PDF...";
+                        $contenidoOK = "pdf";
+                        $archivo_validar_pdf = true;
+                            //copy($path_root.$url_."/".$codigo_institucion."/".$nombreArchivo,$path_root.$url_.$codigo_institucion."/".$large."/".$nombreArchivo);
+                            //copy($path_root.$url_."/".$codigo_institucion."/".$nombreArchivo,$path_root.$url_.$codigo_institucion."/".$small."/".$nombreArchivo);
                     }
+            // validar si el archivo es PDF O IMAGEN
+            if($archivo_validar_pdf == false){
+                $mensajeError = "Cargado Archivo IMAGEN...";
+                $contenidoOK = "img";
                 //  OBETNER COORD3ENADAS ANCHO Y ALTO.
                     $ancho_original = imagesx( $original );
                     $alto_original = imagesy( $original );
@@ -101,13 +118,19 @@ if (is_array($_FILES) && count($_FILES) > 0) {
                         //  ****************************************************************************************************************
                         //  ****************************************************************************************************************
                 // UTILIZACIÓN DE LAS HERRAMIENTAS GD CON IMAGE.
-
+            }
             // Guardar el nombre de la imagen. en la tabla.            
             // Armar query. para actualizar el nombre del archivo de la ruta foto.
                 $query = "UPDATE personal_portafolio SET url_imagen = '".$nombreArchivo."' WHERE id_ = ". $id_portafolio;
             // Ejecutamos el Query.
                 $consulta = $dblink -> query($query);
                     echo "..".$url_.$id_personal."/".$nombreArchivo;
+                    // Armamos array para convertir a JSON
+                    $salidaJson = array("respuesta" => $respuestaOK,
+                        "mensaje" => $mensajeError,
+                        "url" => $url_archivo,
+                        "contenido" => $contenidoOK);
+                    echo json_encode($salidaJson);
         } else {
             echo 0;
         }
