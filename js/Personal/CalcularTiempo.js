73e755976a44845e0a5754544fde78b0214e2692
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2023-10-11 15:41:42 -0600

    update-varios

--- js/Personal/CalcularTiempo.js
+++ js/Personal/CalcularTiempo.js
@@ -1,4 +1,3 @@
-$(document).ready(function () {
 /*----------Funcion para obtener el tiempo en dias y horas------------*/
 function calcular_tiempo_12_24() {
 // variables
@@ -44,43 +43,17 @@ function calcular_tiempo_12_24() {
   } else {
     meridian_1 = 'PM';
   }
-  //alert(hours + ':' + minutes + ' ' + meridian);
-  
-  $("label[for='LblHoraDesde']").text(hours + ':' + minutes + ' ' + meridian);
-  $("label[for='LblHoraHasta']").text(hours_1 + ':' + minutes_1 + ' ' + meridian_1);
+// Enviar datos al Span
+  $("#SpanHoraDesde").text(hours + ':' + minutes + ' ' + meridian);
+  $("#SpanHoraHasta").text(hours_1 + ':' + minutes_1 + ' ' + meridian_1);
 }
 /*----------Funcion para obtener el tiempo en dias y horas------------*/
 function calcular_tiempo() {
-  /*----------Funcion PARA EL FORMATO 12 HORAS------------*/
-  /*timeSplit = $('#hora_1_hasta').val();
-  timeSplit = timeSplit.split(':');
-  var hours = 0;
-  var meridian = "";
-  var minutes = "";
-  // matriz
-    hours = timeSplit[0];
-    minutes = timeSplit[1];
-    if (hours > 12) {
-      meridian = 'PM';
-      hours -= 12;
-    } else if (hours < 12) {
-      meridian = 'AM';
-      if (hours == 0) {
-        hours = 12;
-      }
-    } else {
-      meridian = 'PM';
-    }
-    // Reemplazar label.
-      $("label[for='end-time_2']").text(hours + ':' + minutes + ' ' + meridian);
-/*----------Funcion PARA EL FORMATO 12 HORAS------------*/
-
 // valor de la fecha
-var tiempo_1_desde = $('#hora_1_desde').val();
-var tiempo_1_hasta = $('#hora_1_hasta').val();
+var tiempo_1_desde = $('#HoraDesde').val();
+var tiempo_1_hasta = $('#HoraHasta').val();
 var dia = 0;
-
-var codigo_tipo_contratacion = $('#lstContratacion_1 option:selected').val();
+var codigo_tipo_contratacion = $('#lstTipoContratacion option:selected').val();
 codigo_tipo_contratacion = codigo_tipo_contratacion.substr(0,2);
 // VALIDAR CON RESPECTO A LA CONDICIONAL DE LA CONTRATACIÓN.
     if(codigo_tipo_contratacion == "05"){
@@ -109,19 +82,14 @@ codigo_tipo_contratacion = codigo_tipo_contratacion.substr(0,2);
           dia = 1;
           horas = 0;
           minutos = 0;
-          tiempo_calculado_1 = dia;
+          tiempo_calculado_1 = dia + " Dia(s)";
         }
-        
+        // Menor a 8 horas        
         if(horas < 8){
-          
-          tiempo_calculado_1 = horas+":"+minutos;
-          }
-      
+          tiempo_calculado_1 = horas + " Horas " + minutos + " Minutos";
+        }
       // Pasar los valores.
-        $("#tiempo_calculado_1").val(tiempo_calculado_1);
-        $("#dia").val(dia);
-        $("#hora").val(horas);
-        $("#minutos").val(minutos);
+        $("#SpanDiasHoras").text(tiempo_calculado_1);  
     }else{
       inicioMinutos = parseInt(tiempo_1_desde.substr(3,2));
       inicioHoras = parseInt(tiempo_1_desde.substr(0,2));
@@ -139,28 +107,23 @@ codigo_tipo_contratacion = codigo_tipo_contratacion.substr(0,2);
         
         horas = transcurridoHoras.toString();
         minutos = transcurridoMinutos.toString();
-        
+        //
         if (horas.length < 2) {
           horas = ""+horas;
         }
-      
+      //
         if(horas == 5){
           dia = 1;
           horas = 0;
           minutos = 0;
           tiempo_calculado_1 = dia;
         }
-        
+        ///        
         if(horas < 5){
-          
           tiempo_calculado_1 = horas+":"+minutos;
           }
-      
       // Pasar los valores.
-        $("#tiempo_calculado_1").val(tiempo_calculado_1);
-        $("#dia").val(dia);
-        $("#hora").val(horas);
-        $("#minutos").val(minutos);
+        $("#SpanDiasHoras").text(tiempo_calculado_1 + ":" + horas + ":" + minutos);
     }
 }
 
@@ -209,6 +172,3 @@ finHoras = parseInt(tiempo_1_hasta.substr(0,2));
   $("#hora_m").val(horas);
   $("#minutos_m").val(minutos);
   }
-      // REACULTAR A FORMATO 12 H LABEL.
-    calcular_tiempo_12_24();
-});
