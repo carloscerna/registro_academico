eb8f8a4870330c6032c0a9b0a3d807166ff5fb58
carloscerna <carlos_wilfredo_cerna@hotmail.com>
2024-11-29 13:33:21 -0600

    udpate-varios

--- includes/backup-10391.bat
+++ includes/backup-10391.bat
@@ -2,7 +2,7 @@ cls
 SET PGPASSWORD=Orellana
 pg_dump -U postgres -v -F c registro_academico_10391 > c:\wamp64\www\registro_academico_10391.dump
 cls
-xcopy c:\wamp64\www\registro_academico_10391.dump "H:\Mi unidad\10391\respaldo" /y
+xcopy c:\wamp64\www\registro_academico_10391.dump "G:\Mi unidad\CE10391\respaldo" /y
 xcopy c:\wamp64\www\registro_academico_10391.dump "D:\CE10391\respaldo" /y
 
 xcopy c:\wamp64\www\registro_academico_10391.dump "C:\Users\MINEDUCYT\OneDrive - Ministerio de Educación, Gobierno de El Salvador\CE10391\Respaldo" /Y
